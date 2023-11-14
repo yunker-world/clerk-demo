5435c4ea2b7c929b1ee283dc4a2c36fde1e9b3b4
yunker-world <yunker.world@gmail.com>
2023-11-14 16:16:13 +0800

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/api/webhooks(.*)"],
+  ignoredRoutes: ["/api/webhook"],
 });
 
 export const config = {
